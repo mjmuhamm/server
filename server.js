171081dfde86636126f3620cf192037bedf30a19
Malik Muhammad <mjmuhamm93@gmail.com>
2023-06-10 11:54:44 -0400

    Additions

--- server.js
+++ server.js
@@ -8,7 +8,19 @@ const { initializeApp } = require('firebase-admin/app');
 
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./bloompayments/bloom-4e1f2-firebase-adminsdk-agp88-8c599ea0b3.json");
+const serviceAccount = {
+  'type': 'service_account',
+  'project_id': "bloom-4e1f2",
+  'private_key_id': process.env.PRIVATE_KEY_ID,
+  // See: https://stackoverflow.com/a/50376092/3403247.
+  'private_key': (process.env.PRIVATE_KEY as string).replace(/\\n/g, '\n'),
+  'client_email': process.env.CLIENT_EMAIL,
+  'client_id': process.env.CLIENT_ID,
+  'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
+  'token_uri': 'https://oauth2.googleapis.com/token',
+  'auth_provider_x509_cert_url': process.env.PROVIDER_X509_CERT_URL,
+  'client_x509_cert_url': process.env.CLIENT_X509_CERT_URL,
+} as ServiceAccount;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
