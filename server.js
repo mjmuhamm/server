e90fda2c2b0a81c0edbefaafe8ab8ed4ea371921
Malik Muhammad <mjmuhamm93@gmail.com>
2023-06-19 15:40:51 -0400

    Additions

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')('sk_test_51J1HegHO46FqqdfmowFPCg4CEsyu4Lh08uTmZOEcOIv7S2gZoY4pfwvUwmSJ5mAPJDS0ZYlCHaGWrdFeGJgWa5YB00xgtRQrRZ');
+const stripe = require('stripe')('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');
 // This example sets up an endpoint using the Express framework.
 // Watch this video to get started: https://youtu.be/rPR2aJ6XnAc.
 //sk_test_51J1HegHO46FqqdfmowFPCg4CEsyu4Lh08uTmZOEcOIv7S2gZoY4pfwvUwmSJ5mAPJDS0ZYlCHaGWrdFeGJgWa5YB00xgtRQrRZ
@@ -553,7 +553,7 @@ app.post('/create-payment-intent', async (req, res) => {
     ephemeralKey: ephemeralKey.secret,
     customer: customer.id,
     paymentId: paymentIntent.id,
-    publishableKey: 'pk_test_51J1HegHO46FqqdfmVCS75Zl7XsGfbSCMa3KI2lNn3uc4MEvD4lC604d8Yy4NMrMy8feErjy9n24FlezeQtyFtbyM00N1x69Xuo'
+    publishableKey: 'pk_live_51J1HegHO46FqqdfmsaC7SmYsGcigxAbvU2b7p5oDqEIPUbUj47pvmMNKPJ9PrZjqjeM3743ANM23VlByqUVpun6X00VqpDpsTB'
   });
 
 
